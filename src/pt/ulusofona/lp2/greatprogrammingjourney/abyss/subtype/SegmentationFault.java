16953808c66fa2dcb13ff36e2f79482bc6fa5d08
Duarte Martins <iduartemt05.04@gmail.com>
2025-11-30 03:18:21 +0000

    Implemtacao SegmentationFault

--- src/pt/ulusofona/lp2/greatprogrammingjourney/abyss/subtype/SegmentationFault.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/abyss/subtype/SegmentationFault.java
@@ -2,8 +2,12 @@ package pt.ulusofona.lp2.greatprogrammingjourney.abyss.subtype;
 
 import pt.ulusofona.lp2.greatprogrammingjourney.Board;
 import pt.ulusofona.lp2.greatprogrammingjourney.EventType;
+import pt.ulusofona.lp2.greatprogrammingjourney.Slot;
 import pt.ulusofona.lp2.greatprogrammingjourney.abyss.Abyss;
 import pt.ulusofona.lp2.greatprogrammingjourney.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SegmentationFault extends Abyss {
     public SegmentationFault(int position) {
@@ -13,5 +17,26 @@ public class SegmentationFault extends Abyss {
     @Override
     public void playerInteraction(Player player, Board board) {
 
+        for (Slot slot : board.getSlots()) {
+            if (slot.getPlayers().contains(player)) {
+
+                int destinySlot = slot.getNrSlot() - 3;
+
+                if (destinySlot < 1) {
+                    destinySlot = 1;
+                }
+
+                Slot destinationSlot = board.encontraSlot(destinySlot);
+
+                if (slot.getPlayers().size() >= 2) {
+                    List<Player> vitimas = new ArrayList<>(slot.getPlayers());
+
+                    for (Player p : vitimas) {
+                        slot.removePlayer(p);
+                        destinationSlot.addPlayer(p);
+                    }
+                }
+            }
+        }
     }
 }
