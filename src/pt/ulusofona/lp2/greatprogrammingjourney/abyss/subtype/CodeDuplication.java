377ea6a2e0de82ec92e5e0c2b289b56f61e8d8b5
Duarte Martins <iduartemt05.04@gmail.com>
2025-12-08 18:18:36 +0000

    alteraoes alt14

--- src/pt/ulusofona/lp2/greatprogrammingjourney/abyss/subtype/CodeDuplication.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/abyss/subtype/CodeDuplication.java
@@ -15,7 +15,7 @@ public class CodeDuplication extends Abyss {
 
     @Override
     public String playerInteraction(Player player, Board board) {
-        Tool inheritance = null;
+        /*Tool inheritance = null;
 
         for (Tool t : player.getTools()) {
             if (t.getId() == 0) {
@@ -45,7 +45,7 @@ public class CodeDuplication extends Abyss {
         Slot destinationSlot = board.encontraSlot(currentSlot.getNrSlot() - player.getLastDiceValue());
 
         currentSlot.removePlayer(player);
-        destinationSlot.addPlayer(player);
+        destinationSlot.addPlayer(player);*/
         return "Recua para a casa anterior";
     }
 }
